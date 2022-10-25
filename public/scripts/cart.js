26707656f07a9b00a2b818738eed201775ea4616
lucadard <lucadardenne@hotmail.com>
2022-10-25 17:39:46 -0300

    delete unused functions

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,15 +1,4 @@
 //CARRITO
-const cart = document.getElementById('cart')
-const productsCounter = document.getElementById('counter')
-const totalPrice = document.getElementById('price')
-let price = 0
-
-async function getCart() {
-  const res = await fetch(`/api/carts/${cartId}/products`)
-  const data = await res.json()
-  return data.cartProducts
-}
-
 async function deleteProductFromCart(id, userId, deleteAll = true) {
   if (userId === '') return console.log('not logged')
   const res = await fetch(
