c50529dea3524ad892b2fa0e8c4b41ad460eae45
lucadard <lucadardenne@hotmail.com>
2022-11-02 12:06:38 -0300

    copy fn from forked js file to work inside ts

--- src/routes/randoms/router.ts
+++ src/routes/randoms/router.ts
@@ -2,17 +2,39 @@ import { Router } from 'express'
 import { fork } from 'child_process'
 import path from 'path'
 
+import { argumentsObject } from '../../config/args'
+
+const calculate = (amount: number) => {
+  const randoms: { [key: string]: number } = {}
+
+  const startTime = performance.now()
+  for (let i = 0; i < amount; i++) {
+    const randomNumber = Math.ceil(Math.random() * (1000 + 1) - 1)
+    if (randoms[randomNumber]) randoms[randomNumber]++
+    else randoms[randomNumber] = 1
+  }
+  const endTime = performance.now()
+
+  return {
+    randoms,
+    time: `in ${((endTime - startTime) / 1000).toFixed(3)}s`,
+    by: `Worker ${process.pid}`
+  }
+}
+
 export const router = Router()
 
 router.get('/', ({ query }, res) => {
   let cant: number = query.cant ? +query.cant : 100000000
 
-  const randoms = fork(
-    path.resolve(__dirname, '../src/routes/randoms/calculate.js')
-  )
-
-  randoms.on('message', (data) => {
-    if (data === 'ready') randoms.send(cant)
-    else res.json(data)
-  })
+  if (argumentsObject.mode === 'cluster') res.json(calculate(cant))
+  else {
+    const randoms = fork(
+      path.resolve(__dirname, '../src/routes/randoms/calculate.js')
+    )
+    randoms.on('message', (data) => {
+      if (data === 'ready') randoms.send(cant)
+      else res.json(data)
+    })
+  }
 })
