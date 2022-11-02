4ebdd87f2d17fd7d51dd8d8b4cb1f28482e35c04
lucadard <lucadardenne@hotmail.com>
2022-11-02 13:33:46 -0300

    add support to --i (instance) argument

--- src/config/args.ts
+++ src/config/args.ts
@@ -3,12 +3,22 @@ import parseArgs from 'minimist'
 type arguments = {
   port: number
   mode: 'cluster' | 'fork'
+  i: number
 }
 
 const argumentsObject = parseArgs<arguments>(process.argv.slice(2))
 
-if (!argumentsObject.port) argumentsObject.port = 8080
-if (!argumentsObject.mode || argumentsObject.mode !== 'cluster')
+if (!argumentsObject.port || isNaN(argumentsObject.port))
+  argumentsObject.port = 8080
+
+if (!argumentsObject.i || isNaN(argumentsObject.i)) argumentsObject.i = 5
+else {
+  if (argumentsObject.i < 2) argumentsObject.i = 2
+}
+
+if (!argumentsObject.mode || argumentsObject.mode !== 'cluster') {
   argumentsObject.mode = 'fork'
+  argumentsObject.i = 1
+}
 
 export { argumentsObject }
