c77996bf8221b09de2bc1be5ee89278799e0f37a
lucadard <lucadardenne@hotmail.com>
2022-11-09 11:45:52 -0300

    add warn transport & fix petition function

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -8,12 +8,16 @@ export const logger: any = winston.createLogger({
       level: 'debug'
     }),
     new winston.transports.File({
+      filename: './logs/warn.log',
+      level: 'warn'
+    }),
+    new winston.transports.File({
       filename: './logs/errors.log',
       level: 'error'
     })
   ]
 })
 
-logger.petition = (type = 'info', req: any) => {
+logger.petition = (req: any, type = 'info') => {
   logger.log(type, `Request type ${req.method} to ${req.url}`)
 }
