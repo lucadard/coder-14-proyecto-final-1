537f21fdee441414b2a51c329548a3b5714bc617
lucadard <lucadardenne@hotmail.com>
2022-11-10 14:47:03 -0300

    change main to not fork one worker in fork mode and use that instace as the old worker

--- main.ts
+++ main.ts
@@ -4,36 +4,35 @@ import { createServer } from './src/server'
 import { argumentsObject } from './src/config/args'
 import { logger } from './src/config/logger'
 
+async function initializeServer() {
+  const PORT =
+    argumentsObject.mode === 'fork' ? argumentsObject.port : +process.env.PORT!
+  try {
+    const server = await createServer(PORT)
+    logger.info(
+      `Worker ${cluster.worker?.id}, pid: ${process.pid}: Listening on port ${
+        server.address().port
+      }`
+    )
+  } catch (err) {
+    logger.error('Server error', err)
+  }
+}
+
 if (cluster.isPrimary) {
   logger.info(`Program starting...`)
   logger.info(`Running in ${argumentsObject.mode} mode.`)
 
-  for (let i = 0; i < argumentsObject.i; i++) {
-    const PORT =
-      i === 0 ? argumentsObject.port + i : argumentsObject.port + i + 1
-    cluster.fork({ PORT })
-  }
+  if (argumentsObject.mode === 'cluster') {
+    for (let i = 0; i < argumentsObject.i; i++) {
+      const PORT =
+        i === 0 ? argumentsObject.port + i : argumentsObject.port + i + 1
+      cluster.fork({ PORT })
+    }
+  } else initializeServer()
 
   cluster.on('exit', (worker) => {
-    logger.error(`Worker ${worker.id}, pid: ${worker.process.pid}. Died`)
+    logger.error(`Worker ${worker.id || 0}, pid: ${worker.process.pid}. Died`)
     cluster.fork({ PORT: argumentsObject.port + worker.id - 1 })
   })
-} else {
-  async function initializeServer() {
-    const PORT =
-      argumentsObject.mode === 'fork'
-        ? argumentsObject.port
-        : +process.env.PORT!
-    try {
-      const server = await createServer(PORT)
-      logger.info(
-        `Worker ${cluster.worker?.id}, pid: ${process.pid}: Listening on port ${
-          server.address().port
-        }`
-      )
-    } catch (err) {
-      logger.error('Server error', err)
-    }
-  }
-  initializeServer()
-}
+} else initializeServer()
