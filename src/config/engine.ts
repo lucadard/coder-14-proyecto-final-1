b82805fdeb5916f035d490890e51727b2f98811f
lucadard <lucadardenne@hotmail.com>
2022-11-24 10:26:54 -0300

    change relative path depending on node env

--- src/config/engine.ts
+++ src/config/engine.ts
@@ -1,8 +1,10 @@
 import { engine } from 'express-handlebars'
 import path from 'path'
 
+const relativePath = process.env.NODE_ENV === 'production' ? '..' : '../..'
+
 export const hbsConfig = engine({
   extname: '.hbs',
-  defaultLayout: path.join(__dirname, '../views/layouts/main.hbs'),
-  layoutsDir: path.join(__dirname, '../views/layouts')
+  defaultLayout: path.join(__dirname, `${relativePath}/views/layouts/main.hbs`),
+  layoutsDir: path.join(__dirname, `${relativePath}/views/layouts`)
 })
