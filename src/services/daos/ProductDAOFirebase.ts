fbb4f04a564fa1b0949e966460beea4b0af34c1c
lucadard <lucadardenne@hotmail.com>
2022-11-24 10:17:46 -0300

    deleted files

--- src/services/daos/ProductDAOFirebase.ts
+++
@@ -1,9 +0,0 @@
-import ContenedorFirebase from '../contenedores/ContenedorFirebase'
-
-import { Product } from '../../types'
-
-export default class ProductDAO extends ContenedorFirebase<Product> {
-  constructor() {
-    super('products')
-  }
-}
