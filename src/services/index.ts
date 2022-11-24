63ec2eab08481eac8301e029740c0c917c025ff0
lucadard <lucadardenne@hotmail.com>
2022-11-24 10:26:04 -0300

    only export necessary classes instances

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,49 +1,7 @@
-import ProductDAOMongoDB from './daos/ProductDAOMongoDB'
-import CartDAOMongoDB from './daos/CartDAOMongoDB'
+import ProductDAO from './daos/ProductDAO'
+import CartDAO from './daos/CartDAO'
 import UserDAO from './daos/UserDAO'
 
-import ProductDAOFirebase from './daos/ProductDAOFirebase'
-import CartDAOFirebase from './daos/CartDAOFirebase'
-
-import ProductDAOMemoria from './daos/ProductDAOMemoria'
-import CartDAOMemoria from './daos/CartDAOMemoria'
-
-import ProductDAOArchivo from './daos/ProductDAOArchivo'
-import CartDAOArchivo from './daos/CartDAOArchivo'
-
-// const productDAOMongoDB = new ProductDAOMongoDB()
-// const cartDAOMongoDB = new CartDAOMongoDB()
-
-// const productDAOFirebase = new ProductDAOFirebase()
-// const cartDAOFirebase = new CartDAOFirebase()
-
-// const productDAOMemoria = new ProductDAOMemoria()
-// const cartDAOMemoria = new CartDAOMemoria()
-
-// const productDAOArchivo = new ProductDAOArchivo()
-// const cartDAOArchivo = new CartDAOArchivo()
-
-export const exportedDAOs = (
-  DAO: 'archivo' | 'memoria' | 'mongodb' | 'firebase'
-): any => {
-  let cartDAO, productDAO
-  if (DAO === 'archivo') {
-    cartDAO = new CartDAOArchivo()
-    productDAO = new ProductDAOArchivo()
-  } else if (DAO === 'mongodb') {
-    cartDAO = new CartDAOMongoDB()
-    productDAO = new ProductDAOMongoDB()
-    // } else if (DAO === 'firebase') {
-    //   cartDAO = new CartDAOFirebase()
-    //   productDAO = new ProductDAOFirebase()
-  } else {
-    cartDAO = new CartDAOMemoria()
-    productDAO = new ProductDAOMemoria()
-    cartDAO.addOne({ id: 1, timestamp: +new Date(), products: [] })
-  }
-  return {
-    cartDAO,
-    productDAO
-  }
-}
+export const productDAO = new ProductDAO()
+export const cartDAO = new CartDAO()
 export const userDAO = new UserDAO()
